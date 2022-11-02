cf94bf8059a9ad04489934131e75e42da326432d
ZaidP6 <pilarads06@gmail.com>
2022-11-02 22:25:51 +0100

    2 nov 22 22.30

---
+++ Lenguaje de marca/Tema 2/Recursos/Videos/Ruroc ATLAS 4.0 DC The Batman Casco de moto integral_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C03FL3TSDURFOPG0ISUG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C03FL3TSDURFOPG0ISUG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C03FL3TSDURFOPG0ISUG").setPixelInfo({status: 0, name: "Ruroc (Developer Mode)", advertiserID: "6851492086282190854", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("ruroc.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig(null);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
