9f81fdf29a555964a98650f851e6cf4c33d4b2e9
zaidp6 <pilarads06@gmail.com>
2022-11-03 10:59:41 +0100

    base datos ej04

---
+++ Base de datos/Ejercicios/T3/04_climatologia.sql
@@ -0,0 +1,6 @@
+--select estacion from climatologia
+--select Julio or Agosto from climatologia
+--select fecha where fecha == julio or agosto from climatologia
+--select fecha where julio or agosto == mes & 2019 ==ano
+--where provincia from climatologia
+select * from climatologia where fecha between 2019-07-01 and 2019-08-31
