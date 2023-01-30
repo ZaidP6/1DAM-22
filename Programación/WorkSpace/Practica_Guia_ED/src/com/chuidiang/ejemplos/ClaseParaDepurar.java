10b37b0af03610ffe66c6f23cc6ba31ab45cc5f7
Zaid <pilarads06@gmail.com>
2023-01-30 11:33:46 +0100

    30 ene 11.30

--- Programación/WorkSpace/Practica_Guia_ED/src/com/chuidiang/ejemplos/ClaseParaDepurar.java
+++ Programación/WorkSpace/Practica_Guia_ED/src/com/chuidiang/ejemplos/ClaseParaDepurar.java
@@ -14,7 +14,7 @@ public class ClaseParaDepurar {
 	public double echaCuentas() {
     	lista = new LinkedList<Double>();
     	for (int i = 0; i<100;i++){
-        	lista.add((double)i);
+               	lista.add((double)i);
     	}
     	suma = 0.0;
     	for (Double valor : lista){
