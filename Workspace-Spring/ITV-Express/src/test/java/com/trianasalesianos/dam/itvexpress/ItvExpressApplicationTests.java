1dbd7f7d80a95c58c68c863dc02a0acc87ed45e7
Zaid <pilarads06@gmail.com>
2023-04-21 13:35:20 +0200

    21 abril 14.30

---
+++ Workspace-Spring/ITV-Express/src/test/java/com/trianasalesianos/dam/itvexpress/ItvExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trianasalesianos.dam.itvexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItvExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
